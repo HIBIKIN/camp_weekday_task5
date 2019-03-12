3c78939ea1d06267034bcfdcac3ef9be328a64e6
HIBIKIN <hi3bi1ki5@gmail.com>
2019-03-12 19:18:53 +0900

    リファクタリング

--- app/helpers/fortunetellings_helper.rb
+++ app/helpers/fortunetellings_helper.rb
@@ -9,29 +9,29 @@ module FortunetellingsHelper
   def constellation(month, day)
     month_day = (month + day).to_i
     if month_day.between?(120, 218)
-      "水瓶座"
+      "水瓶"
     elsif month_day.between?(219, 320)
-      "魚座"
+      "魚"
     elsif month_day.between?(321, 419)
-      "牡羊座"
+      "牡羊"
     elsif month_day.between?(420, 520)
-      "牡牛座"
+      "牡牛"
     elsif month_day.between?(521, 621)
-      "双子座"
+      "双子"
     elsif month_day.between?(622, 722)
-      "蟹座"
+      "蟹"
     elsif month_day.between?(723, 822)
-      "獅子座"
+      "獅子"
     elsif month_day.between?(823, 922)
-      "乙女座"
+      "乙女"
     elsif month_day.between?(923, 1023)
-      "天秤座"
+      "天秤"
     elsif month_day.between?(1024, 1122)
-      "蠍座"
+      "蠍"
     elsif month_day.between?(1123, 1221)
-      "射手座"
+      "射手"
     elsif month_day.between?(1222, 1231) || month_day.between?(101, 119)
-      "山羊座"
+      "山羊"
     end
   end
 
