600bebf1f4d1a22f7bbd2018190ba95a01a159cb
HIBIKIN <hi3bi1ki5@gmail.com>
2019-03-12 17:04:16 +0900

    ルートの追加と、誕生日の表示、今日が誕生日か？

--- app/controllers/fortunetellings_controller.rb
+++ app/controllers/fortunetellings_controller.rb
@@ -1,4 +1,13 @@
 class FortunetellingsController < ApplicationController
+  include FortunetellingsHelper
+
   def show
+    date = params[:date]
+    @year = date[0,4]
+    @month = date[4,2]
+    @day = date[6,2]
+    #今日が誕生日か？
+    @birthday = birthday?(date)
+    
   end
 end
--- app/helpers/fortunetellings_helper.rb
+++ app/helpers/fortunetellings_helper.rb
@@ -1,2 +1,7 @@
 module FortunetellingsHelper
+
+  #今日が誕生日ならtrue
+  def birthday?(date)
+    date[4,4] == Date.today.strftime("%m%d")
+  end
 end
